cdc091d2a071d44e8777b6f4b712122791e8ad6c
dodofk <ricky.16811@gmail.com>
2023-05-06 14:36:39 +0800

    fix typo

--- preprocess/repo_clustering.py
+++ preprocess/repo_clustering.py
@@ -134,7 +134,7 @@ elif args.model_name in [
             attention_mask = inputs.attention_mask.unsqueeze(-1)
             embeddings = (
                 torch.sum(outputs.last_hidden_state * attention_mask, dim=1)
-                / torch.sum(attention_mask, dim=1).
+                / torch.sum(attention_mask, dim=1)
             ).cpu().numpy()
         elif args.strategy == "cls":
             embeddings = outputs.last_hidden_state[:, 0, :].cpu().numpy()
